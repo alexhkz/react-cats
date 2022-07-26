0b29067e455fe855f0f06859ab993e7333653e7e
alexhkz <alexwebcode@gmail.com>
2022-07-26 20:05:30 +0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import React from "react";
+
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import CatBlock from './components/CatBlock';
+
+import './scss/app.scss';
+
 function App() {
 	return (
-		<div className="App">
-			<h1>Hello world!</h1>
+		<div className="wrapper">
+			<Header/>
+			<div className="content">
+			<div className="container">
+				<div className="content__top">
+					<Categories/>
+					<Sort/>
+				</div>
+				<h2 className="content__title">Все котики</h2>
+				<div className="content__items">
+					<CatBlock title='Британская' price={500} />
+					<CatBlock title='Абиссин' price={400} />
+					<CatBlock title='Русская голубая' price={350} />
+					<CatBlock title='Оцикет' price={600} />
+					<CatBlock title='Персидская' price={550} />
+					<CatBlock title='Сиамская' price={450} />
+				</div>
+			</div>
+			</div>
 		</div>
 	);
 }
---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+function Categories() {
+	return (
+		<div className="categories">
+			<ul>
+				<li className="active">Все</li>
+				<li>Чёрные</li>
+				<li>Белые</li>
+				<li>Серые</li>
+				<li>Рыжие</li>
+				<li>Разноцветные</li>
+			</ul>
+		</div>
+	)
+}
+
+export default Categories;

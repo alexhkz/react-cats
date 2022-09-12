373df21f2be35517cb44a3296fff2e388ff5569b
Alexander Stojkov <alexwebcode@gmail.com>
2022-09-12 17:01:30 +0300

    add store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { createContext, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Routes, Route } from 'react-router-dom';
+import { increment, decrement } from "./redux/slices/filterSlice";
 
 import Header from "./components/Header";
 import Home from "./pages/Home";
@@ -12,9 +14,24 @@ export const SearchContext = createContext();
 
 function App() {
 	const [searchValue, setSearchValue] = useState('');
+	const count = useSelector((state) => state.counter.count)
+ 	const dispatch = useDispatch()
 
 	return (
 		<div className="wrapper">
+			<button
+				aria-label="Increment value"
+				onClick={() => dispatch(increment())}
+			>
+				Increment
+			</button>
+			<span>{count}</span>
+			<button
+				aria-label="Decrement value"
+				onClick={() => dispatch(decrement())}
+			>
+				Decrement
+			</button>
 			<SearchContext.Provider value={{searchValue, setSearchValue}}>
 				<Header />
 				<div className="content">
